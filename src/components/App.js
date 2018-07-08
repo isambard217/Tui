df8010ed4998f5ddaf3bcc0c405d02f3943aad77
Isambard Wheeler-artwell <isambardwheeler-artwell@isambards-air.home>
2018-07-08 19:30:25 +0100

    imported caseForm into App.js file

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+// Components
+import caseForm from 'components/caseFormComponent/caseForm';
 
 export default () => {
     return (
         <div>
             Im the app
-         </div>
+            <caseForm/>
+        </div>
     );
 };
