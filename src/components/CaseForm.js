8f7e8eadd8555283720123fcbf9da3f252d8d424
Isambard Wheeler-artwell <isambardwheeler-artwell@isambards-air.home>
2018-07-08 20:22:20 +0100

    Finished simple CaseForm view

--- src/components/CaseForm.js
+++ src/components/CaseForm.js
@@ -3,10 +3,32 @@ import React, { Component } from 'react';
 class CaseForm extends Component {
 
     render() {
-        return(
-            <p> Case Form </p>
+        return (
+            <form>
+                <h4> Add a Case </h4>
+                <textarea />
+                <div>
+                    <button>Submit Comment</button>
+                </div>
+            </form>
         );
     }
 }
 
 export default CaseForm;
+
+/*
+
+
+ <form onSubmit={this.handleSubmit}>
+                <h4>Add a Comment </h4>
+                <textarea onChange={this.handleChange} value={this.state.comment}/>
+                <div>
+                    <button>Submit Comment</button>
+                </div>
+            </form>
+
+
+
+
+*/
