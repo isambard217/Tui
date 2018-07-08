1bace9378c2e82f6e034078cff2c4ce097f6efbf
Isambard Wheeler-artwell <isambardwheeler-artwell@isambards-air.home>
2018-07-08 22:52:06 +0100

    improved test scenario

--- src/components/CaseFormComponent/__test__/CaseForm.test.js
+++ src/components/CaseFormComponent/__test__/CaseForm.test.js
@@ -7,14 +7,14 @@ import CaseForm from 'components/CaseFormComponent/CaseForm';
 let wrapped;
 
 beforeEach(() => {
-
     wrapped = mount(<CaseForm />);
-
 });
 
 
-it('has a text box', () => {
+it('has an input field and has a button', () => {
 
     expect(wrapped.find('input').length).toEqual(1);
+    expect(wrapped.find('Button').length).toEqual(1);
 
 });
+
