56bf62b28c689bd62f37d0cf1526438472ea8e40
Isambard Wheeler-artwell <isambardwheeler-artwell@isambards-air.home>
2018-07-08 20:48:57 +0100

    Added test file for CaseForm

---
+++ src/components/CaseFormComponent/__test__/CaseForm.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import CaseForm from 'components/CaseFormComponent/CaseForm';
+
+let wrapped;
+
+beforeEach(() => {
+
+    wrapped =
+
+});
+
+it('has a text box', () => {
+
+
+
+});
