74482e6283b57cfa89be782d6a63631e4752a2b2
Isambard Wheeler-artwell <isambardwheeler-artwell@isambards-air.home>
2018-07-08 22:44:26 +0100

    Added caseform test

--- src/components/CaseFormComponent/__test__/CaseForm.test.js
+++ src/components/CaseFormComponent/__test__/CaseForm.test.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { shallow, mount } from 'enzyme';
 
+// Components
 import CaseForm from 'components/CaseFormComponent/CaseForm';
 
 let wrapped;
 
 beforeEach(() => {
 
-    wrapped =
+    wrapped = mount(<CaseForm />);
 
 });
 
+
 it('has a text box', () => {
 
-
+    expect(wrapped.find('input').length).toEqual(1);
 
 });
