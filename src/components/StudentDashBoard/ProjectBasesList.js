06da6e3447d07f8b9df83461211095725a236097
Alhaytham Elhassan <asalhaytham@gmail.com>
2018-08-23 05:06:58 +0100

    Feature/render project bases from back end (#11)
    *  add swagger and lodash dependencies
    
    * now front end render projects from the api
    
    * figure our the server address from the window location

--- src/components/StudentDashBoard/ProjectBasesList.js
+++ src/components/StudentDashBoard/ProjectBasesList.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Swagger from 'swagger-client';
+import _ from 'lodash';
 import { Grid } from 'semantic-ui-react';
 import ProjectBase from './ProjectBase'
 import './projectBasesList.css';
@@ -6,40 +8,35 @@ import './projectBasesList.css';
 class ProjectBasesList extends React.Component {
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {
+      projects: [],
+    };
+  }
+  
+  componentDidMount(){
+    const serverUrl = JSON.stringify(window.location).includes('localhost') ?
+      'http://localhost:8080/v2/api-docs' : 'https://www227.lamp.le.ac.uk/v2/api-docs';
+    Swagger(serverUrl)
+      .then(client =>{
+       return client.apis.templates.getTemplatesUsingGET();
+      })
+      .then(({ body }) => this.setState({ projects: body }))
+      .catch(error => console.log('could not get api client'));
   }
   
   render() {
+    const { projects } = this.state;
     return (
       <Grid className='left aligned equal width fluid' container columns={3}>
-      <ProjectBase
-        ProjectBaseName='Old Man killed in library'
-        ProjectDescription='Description of the porject goes here ...'
-      />
-        <ProjectBase
-          ProjectBaseName='Old Man killed in library'
-          ProjectDescription='Description of the porject goes here ...'
-        />
-        <ProjectBase
-          ProjectBaseName='Old Man killed in library'
-          ProjectDescription='Description of the porject goes here ...'
-        />
-        <ProjectBase
-          ProjectBaseName='Old Man killed in library'
-          ProjectDescription='Description of the porject goes here ...'
-        />
-        <ProjectBase
-          ProjectBaseName='Old Man killed in library'
-          ProjectDescription='Description of the porject goes here ...'
-        />
-        <ProjectBase
-          ProjectBaseName='Old Man killed in library'
-          ProjectDescription='Description of the porject goes here ...'
-        />
-        <ProjectBase
-          ProjectBaseName='Old Man killed in library'
-          ProjectDescription='Description of the porject goes here ...'
-        />
+        {
+          _.map( projects, project => (
+            <ProjectBase
+              key={_.uniqueId()}
+              ProjectBaseName={project.name}
+              ProjectDescription={project.description}
+            />
+          ))
+        }
       </Grid>
     );
   }
